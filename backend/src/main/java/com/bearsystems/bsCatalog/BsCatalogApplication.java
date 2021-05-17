970ec2ab5efe07ee0c8b8e8ac6ecb2acc809e05c
kagi.carvalho <kagi.carvalho@outlook.com>
2021-05-17 15:50:01 -0300

    Project created

---
+++ backend/src/main/java/com/bearsystems/bsCatalog/BsCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.bearsystems.bsCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bearsystems/bsCatalog/BsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bearsystems.bsCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
