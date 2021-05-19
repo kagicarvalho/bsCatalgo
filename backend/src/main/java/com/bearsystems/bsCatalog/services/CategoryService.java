fe57be3abc8948b18ce5cdd089405234179eb745
kagi.carvalho <kagi.carvalho@outlook.com>
2021-05-18 21:36:59 -0300

    Post new category

--- backend/src/main/java/com/bearsystems/bsCatalog/services/CategoryService.java
+++ backend/src/main/java/com/bearsystems/bsCatalog/services/CategoryService.java
@@ -41,5 +41,15 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
+	@Transactional(readOnly = true)
+	public CategoryDTO insert(CategoryDTO dto) {
+
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
+
 
 }
